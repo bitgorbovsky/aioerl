eada6867b3c0fba157b677322d8117be53abdc01
Ivan Bannikov <i.bannikov@csbigroup.ru>
2015-03-31 16:49:31 +0300

    Experimental commit with futures.

--- epmd.py
+++ epmd.py
@@ -1,13 +1,27 @@
 # coding: utf-8
 
 import asyncio
-from protocol import EPMDProtocol
+from protocol import EPMDProtocol, connect, NodeInfo
 
 loop = asyncio.get_event_loop()
 
-epmd = EPMDProtocol(7170, loop)
-coro = loop.create_connection(lambda : epmd, '127.0.0.1', 4369)
+#epmd = EPMDProtocol(7170, loop)
+#coro = loop.create_connection(lambda : epmd, '127.0.0.1', 4369)
 
-loop.run_until_complete(coro)
+conn = loop.run_until_complete(connect(
+    loop,
+    '127.0.0.1',
+    4369,
+    NodeInfo('bit', 7170)
+))
+
+res = loop.run_until_complete(conn.register())
+print(res)
+
+res = loop.run_until_complete(conn.get_names())
+print(res)
+
+
+
 loop.run_forever()
 loop.close()
--- protocol.py
+++ protocol.py
@@ -2,6 +2,7 @@
 
 import re
 import asyncio
+from collections import namedtuple
 
 from bitstring import ConstBitStream
 
@@ -11,9 +12,16 @@ from response import UnknownEPMDResponse, Alive2Response
 match_node_info = re.compile('^name (\w+) at port (\d+)$')
 
 
+NodeInfo = namedtuple('NodeInfo', ['name', 'port'])
+
+
+class EPMDConnectionLost(Exception):
+    pass
+
+
 def parse_node_info(info):
     name, port = match_node_info.match(info.decode('utf-8')).groups()
-    return (name, int(port))
+    return NodeInfo(name, int(port))
 
 
 class EPMDProtocol(asyncio.Protocol):
@@ -24,30 +32,64 @@ class EPMDProtocol(asyncio.Protocol):
     WAIT_FOR_NAMES = 3
 
     def __init__(self, node_port, loop):
-        self.loop = loop
-        self.state = self.INIT
-        self.node_port = node_port
+        self._loop = loop
+        self._state = self.INIT
+        self._node_port = node_port
+        self._waiter = None
+        self._transport = None
+        self._timeout = 500
+        self._is_closed = False
 
     def connection_made(self, transport):
         self._transport = transport
-        self.register()
 
     def data_received(self, data):
         response = self._unpack_epmd_resp(data)
+        self._waiter.set_result(response)
+        self._waiter = None
 
     def eof_received(self):
+        self._is_closed = True
+        if self._waiter:
+            self._waiter.set_exception(EPMDConnectionLost(
+                'Failed connection to EPMD'
+            ))
+            self._waiter.exception()
         return True
 
     def connection_lost(self, exc):
-        self.loop.stop()
+        self._loop.stop()
 
+    def _get_waiter(self):
+        return asyncio.Future(loop=self._loop)
+
+    @asyncio.coroutine
     def register(self):
+        if self._is_closed:
+            raise EPMDConnectionLost('Failed connection to EPMD')
         self.state = self.SENT_REG
-        self.send_epmd_request(Alive2Request(port_no=self.node_port))
+        self._waiter = self._get_waiter()
+        self.send_epmd_request(Alive2Request(port_no=self._node_port))
+        result = yield from asyncio.wait_for(
+            self._waiter,
+            self._timeout,
+            loop=self._loop
+        )
+        return result
 
+    @asyncio.coroutine
     def get_names(self):
+        if self._is_closed:
+            raise EPMDConnectionLost('Failed connection to EPMD')
         self.state = self.WAIT_FOR_NAMES
+        self._waiter = self._get_waiter()
         self.send_epmd_request(NamesRequest())
+        result = yield from asyncio.wait_for(
+            self._waiter,
+            self._timeout,
+            loop=self._loop
+        )
+        return result
 
     def _unpack_epmd_resp(self, data):
         buf = ConstBitStream(data)
@@ -57,7 +99,7 @@ class EPMDProtocol(asyncio.Protocol):
             for nodeinfo in buf.bytes[4:].split(b'\n'):
                 if nodeinfo:
                     nodes.append(parse_node_info(nodeinfo))
-            print(nodes)
+            return nodes
         ptype = buf.read('uint:8')
         if ptype == 121:
             success = buf.read('uint:8')
@@ -69,3 +111,11 @@ class EPMDProtocol(asyncio.Protocol):
     def send_epmd_request(self, request):
         data = request.encode()
         self._transport.write(data)
+        self._transport.write_eof()
+
+
+@asyncio.coroutine
+def connect(loop, epmd_host, epmd_port, node_info):
+    epmd = EPMDProtocol(node_info.port, loop)
+    yield from loop.create_connection(lambda: epmd, epmd_host, epmd_port)
+    return epmd
